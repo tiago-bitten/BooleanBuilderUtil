d67dbaf975ae3825acac656bf797e570e7601d72
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-11-28 20:17:39 -0300

    inital files

---
+++ src/main/java/com/labi/booleanbuilder/BooleanbuilderApplication.java
@@ -0,0 +1,13 @@
+package com.labi.booleanbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooleanbuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooleanbuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labi/booleanbuilder/BooleanbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labi.booleanbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooleanbuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
